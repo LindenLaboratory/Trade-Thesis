dc9794c0643ea05ba4930113ba7f0d64a46fb625
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2025-07-23 09:39:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app)
 SHEET_ID="1HoeLkmtjquTsQ6MHIPxz9Y4_ih4W-f6IH4JrZJjqvIQ"
+blogs=[]
 
 #FUNCTIONS
 timestamp = lambda date: "-".join(reversed(date.split("/")))
@@ -33,7 +34,7 @@ def get_sheet():
 
 #MAINLOOP
   #PREREQS
-print("System Online")
+blogs=get_sheet()
   #FLASK
 @app.route('/')
 def main():
@@ -73,8 +74,51 @@ def main():
   """
 @app.route('/blog')
 def get_blogs():
-  return jsonify(get_sheet())
+  global blogs
+  blogs=get_sheet()
+  return jsonify(blogs)
 @app.route('/featured')
 def featured():
   with open("data.txt") as f:
     return f.readlines()[0].strip()
+@app.route('/user/<string:name>')
+def user_data(name):
+  num_posts=0
+  for i in blogs:
+    if i['username']==name:
+      num_posts+=1
+  return f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>User '{name}'</title>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+  <style>
+    body {{
+      background: #fff;
+      font-family: 'Nunito', sans-serif;
+      margin: 0;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #336699;
+    }}
+    .message {{
+      font-size: 1.5em;
+    }}
+    .highlight {{
+      font-weight: bold;
+      color: #004e92;
+    }}
+  </style>
+</head>
+<body>
+  <div class="message">
+    <h1>User <span class="highlight">{name}</span></h1>
+    <p><b># Posts</b> {num_posts}</p>
+  </div>
+</body>
+</html>
+  """
