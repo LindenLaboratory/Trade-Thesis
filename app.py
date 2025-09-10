53553b2953856d45acd4fa78fbef5ac8bef923c1
Isaac Richardson <134805131+LindenLaboratory@users.noreply.github.com>
2025-09-10 09:17:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def upload(res,code):
         vars__["Result"]=vars_["RETURN"]
     if "TIME" in vars:
         vars__["Timeframe"]=vars_["TIME"]
-    print(vars__)
+    print(vars_,vars__)
     for i, line in enumerate(lines):
         for k,v in vars__.items():
             if line.strip().startswith(f"**{k}:**"):
